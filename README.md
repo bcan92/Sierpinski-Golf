0ee4fe99d48e78ec070b0aebaf3dc5c87443566e
Berkin Can <berkincan92@gmail.com>
2014-07-17 02:10:33 -0700

    Adding code itself
    Self explanatory

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Sierpinski-Golf
 ===============
 
-Random code golf attempt in C#
+Sierpinski triangle algorithm code, influenced by the code golf attempts made with Perl.
+C# doesn't quite get there, but it was a fun attempt nevertheless.
---
+++ src/Program.cs
@@ -0,0 +1,4 @@
+﻿namespace System.Linq{using _=String;class P{static void Main(_[]a){
+int v=1<<int.Parse(a[0]),u=++v;var b=new bool[u];b[0]=1>0;
+for(;v-->1;){_ s=new _(' ',v);Console.WriteLine(s+_.Concat(b.Select(
+n=>n?@"/\":"  ")));b=b.Select((n,k)=>k<=u-v&k>0?n^b[k-1]:n).ToArray();}}}}
