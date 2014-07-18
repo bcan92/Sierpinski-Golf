85e053326552d223df6bc4c16542aa39664d8c37
Berkin Can <berkincan92@gmail.com>
2014-07-17 19:09:48 -0700

    Code reductions
    Changed integer parse function and omitted 'whitespace string'
    declaration. Length reduced by 10 chars or so.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
 ﻿namespace System.Linq{using _=String;class P{static void Main(_[]a){
-int v=1<<int.Parse(a[0]),u=++v;var b=new bool[u];b[0]=1>0;
-for(;v-->1;){_ s=new _(' ',v);Console.WriteLine(s+_.Concat(b.Select(
+int v=1<<a[0][0]-48,u=++v;var b=new bool[u];b[0]=1>0;
+for(;v-->1;){Console.WriteLine(new _(' ',v)+_.Concat(b.Select(
 n=>n?@"/\":"  ")));b=b.Select((n,k)=>k<=u-v&k>0?n^b[k-1]:n).ToArray();}}}}
